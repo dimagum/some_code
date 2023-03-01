71d1c9c36feb9947b6ac65468ff82eefc08dbfa0
Dmitry <daguminov@edu.hse.ru>
2023-03-01 15:45:09 +0300

    tasks 6-7 + documentation

--- Graph.h
+++ Graph.h
@@ -5,6 +5,14 @@
 
 using namespace std;
 
+
+struct Point { double x, y, z; };
+std::ostream& operator << (std::ostream& out, Point p) {
+    std::cout << '(' << p.x << ',' << p.y << ',' << p.z << ')';
+    return out;
+}
+
+
 /*!
  * \brief Это граф!
  * @tparam key_type
@@ -18,20 +26,29 @@ class Graph {
      *
      */
     class Node {
+    public:
         value_type val;
 
         map<key_type, weight_type> edges;
 
-    public:
         Node() = default;
 
         Node(const Node& other) = default;
 
         Node(Node&& other) noexcept = default;
 
+        explicit Node(const Point& point) {
+            val = point;
+        }
+
         Node& operator=(const Node& rhs) = default;
 
         Node& operator=(Node&& rhs) noexcept = default;
+
+        Node& operator=(const Point& point) {
+            val = point;
+            return *this;
+        }
 
         bool empty() const {
             return edges.empty();
@@ -42,6 +59,10 @@ class Graph {
         }
 
         value_type& value() {
+            return val;
+        }
+
+        const value_type& value() const {
             return val;
         }
 
@@ -60,12 +81,38 @@ class Graph {
             return edges.end();
         }
 
+        const_iterator begin() const {
+            return edges.begin();
+        }
+
+        const_iterator end() const {
+            return edges.end();
+        }
+
         const_iterator cbegin() const {
             return edges.cbegin();
         }
 
         const_iterator cend() const {
             return edges.cend();
+        }
+
+        pair<iterator, bool> insert_edge(key_type key, weight_type weight) {
+            return edges.insert(pair<key_type, weight_type>(key, weight));
+        }
+
+        pair<iterator, bool> insert_or_assign_edge(key_type key, weight_type weight) {
+            return edges.insert_or_assign(key, weight);
+        }
+
+
+        bool erase_edge(key_type key) {
+            if (edges.find(key) == edges.end()) {
+                return false;
+            }
+
+            edges.erase(key);
+            return true;
         }
 
     };
@@ -96,6 +143,19 @@ public:
         graph.clear();
     }
 
+    void swap(Graph<key_type, value_type, weight_type>& other) {
+        Graph<key_type, value_type, weight_type> tmp = other;
+        other = *this;
+        *this = tmp;
+    }
+
+    friend void swap(Graph<key_type, value_type, weight_type>& first, Graph& second) {
+        Graph<key_type, value_type, weight_type> tmp;
+        tmp = first;
+        first = second;
+        second = tmp;
+    }
+
     typedef typename map<key_type, Node>::iterator iterator;
     typedef typename map<key_type, Node>::const_iterator const_iterator;
 
@@ -104,6 +164,14 @@ public:
     }
 
     iterator end() {
+        return graph.end();
+    }
+
+    const_iterator begin() const {
+        return graph.begin();
+    }
+
+    const_iterator end() const {
         return graph.end();
     }
 
@@ -117,12 +185,13 @@ public:
 
     size_t degree_in(key_type key) {
         if (graph.find(key) == graph.end()) {
-            throw logic_error("no such node.\n");
+            throw std::logic_error("no node with this key in the graph.");
         }
 
         size_t result = 0;
-        for (auto [key1, node1] : graph) {
-            if (node1.edges.find(key) != node1.edges.end()) {
+
+        for (auto& [node_key, node] : graph) {
+            if (node.edges.find(key) != node.edges.end()) {
                 result++;
             }
         }
@@ -135,7 +204,7 @@ public:
             throw logic_error("no such node.\n");
         }
 
-        return graph[key].edges.size();
+        return graph[key].size();
     }
 
     bool loop(key_type key) {
@@ -165,6 +234,103 @@ public:
 
         return graph.at(key);
     }
+
+    pair<iterator, bool> insert_node(key_type key, value_type val) {
+        if (graph.find(key) != graph.end()) {
+            return pair<iterator, bool>(graph.find(key), false);
+        }
+
+        Node tmp;
+        tmp.value() = val;
+
+        return graph.insert(pair<key_type, Node>(key, tmp));
+    }
+
+    pair<iterator, bool> insert_or_assign_node(key_type key, value_type val) {
+        if (graph.find(key) != graph.end()) {
+            graph[key].value() = val;
+            return pair<iterator, bool>(graph.find(key), false);
+        }
+
+        Node tmp;
+        tmp.value() = val;
+
+        return graph.insert(pair<key_type, Node>(key, tmp));
+    }
+
+    pair<iterator, bool> insert_edge(pair<key_type, key_type> keys, weight_type weight) {
+        key_type key_from = keys.first, key_to = keys.second;
+
+        if (graph.find(key_from) == graph.end()) {
+            throw logic_error("first node is absent\n");
+        }
+
+        if (graph.find(key_to) == graph.end()) {
+            throw logic_error("second node is absent\n");
+        }
+
+        auto [it, flag] = graph[key_from].insert_edge(key_to, weight);
+
+        return pair<iterator, bool>(graph.find(key_from), flag);
+    }
+
+    pair<iterator, bool> insert_or_assign_edge(pair<key_type, key_type> keys, weight_type weight) {
+        key_type key_from = keys.first, key_to = keys.second;
+
+        if (graph.find(key_from) == graph.end()) {
+            throw logic_error("first node is absent\n");
+        }
+
+        if (graph.find(key_to) == graph.end()) {
+            throw logic_error("second node is absent\n");
+        }
+
+        auto [it, flag] = graph[key_from].insert_or_assign_edge(key_to, weight);
+
+        return pair<iterator, bool>(graph.find(key_from), flag);
+    }
+
+
+    void clear_edges() {
+        for (auto [node_key, node] : graph) {
+            node.clear();
+        }
+    }
+
+    bool erase_edges_go_from(key_type key) {
+        if (graph.find(key) == graph.end()) {
+            return false;
+        }
+
+        graph[key].clear();
+        return true;
+    }
+
+    bool erase_edges_go_to(key_type key) {
+        if (graph.find(key) == graph.end()) {
+            return false;
+        }
+
+        for (auto [node_key, node] : graph) {
+            node.erase_edge(key);
+        }
+
+        return true;
+    }
+
+    bool erase_node(key_type key) {
+        if (graph.find(key) == graph.end()) {
+            return false;
+        }
+
+        for (auto [node_key, node] : graph) {
+            node.erase_edge(key);
+        }
+
+        graph.erase(key);
+        return true;
+    }
+
 
 };
 
--- main.cpp
+++ main.cpp
@@ -2,16 +2,141 @@
 #include "Graph.h"
 
 
+template<typename Graph>
+void print(const Graph& graph) {
+    if (graph.empty()) {
+        std::cout << "> This graph is empty!" << std::endl;
+        return;
+    }
+    std::cout << "> Size of graph: " << graph.size() << std::endl;
+    for (const auto& [key, node] : graph) {
+        std::cout << '[' << key << "] stores: " << node.value()
+                  << " and matches with:" << std::endl;
+        for (const auto& [key1, weight] : node)
+            std::cout << "\t[" << key << "]\t with weight: "
+                      << weight << std::endl;
+    }
+}
 
-int main() {
+
+int main() {/*
     Graph<int, int, int> graph;
+    auto [it1, flag1] = graph.insert_node(1, 1);
+    auto [it2, flag2] = graph.insert_node(2, 2);
+    auto [it3, flag3] = graph.insert_node(2, 5);
 
-    cout << boolalpha << graph.empty() << "\n";
+    cout << it2->second.value() << " " << it3->second.value() << "\n";
 
-    graph[1];
+    auto [it4, flag4] = graph.insert_or_assign_node(2, 5);
 
-    cout << boolalpha << graph.empty() << "\n";
+    cout << boolalpha << flag1 << " " << flag2 << " " << flag3 << " " << flag4 << "\n";
+    cout << it4->second.value() << "\n";
 
+    cout << boolalpha << graph[1].empty() << "\n";
+
+    graph.insert_edge({1, 1}, 2);
+
+    cout << boolalpha << graph[1].empty() << "\n";
+
+    graph.insert_edge({2, 1}, 5);
+    graph.insert_edge({1, 2}, 7);
+
+    cout << boolalpha << graph[1].size() << " " << graph[2].size() << "\n";
+
+    graph.erase_edges_go_from(1);
+
+    cout << boolalpha << graph[1].size() << " " << graph[2].size() << "\n";
+
+    graph.clear_edges();
+
+    cout << boolalpha << graph[1].size() << " " << graph[2].size() << "\n";
+
+    cout << graph.size() << "\n";
+
+    graph.erase_node(2);
+
+    cout << graph.size() << "\n";
+    */
+
+    Graph<std::string, Point, double> graph;
+    graph["zero"]; // Заполнится точкой, которая заполнится нулями
+    auto [it1, flag1] = graph.insert_node("first", {1, 1, 1});
+    std::cout << std::boolalpha << flag1 << std::endl; // => true
+    graph["second"]; // Заполнится точкой, которая заполнится нулями
+    auto [it2, flag2] = graph.insert_or_assign_node("second", {2, 2, 2}); //перезаполнит
+    std::cout << std::boolalpha << flag2 << std::endl; // => false
+    graph["third"] = Point{ 3, 3, 3 };
+    auto [it3, flag3] = graph.insert_node("third", {1, 1, 1}); // бездействует
+    std::cout << std::boolalpha << flag3 << std::endl; // => false
+    graph["fourth"]; // Заполнится точкой, которая заполнится нулями
+    graph.at("fourth") = Point{ 4, 4, 4 };
+    try { graph.at("fifth"); }
+    catch (std::exception& ex) { std::cout << ex.what() << std::endl; }
+    auto [it4, flag4] = graph.insert_edge({ "first", "second" }, 44.44);
+    std::cout << std::boolalpha << flag4 << std::endl; // => true
+    auto [it5, flag5] = graph.insert_edge({ "first", "second" }, 55.55);
+    std::cout << std::boolalpha << flag5 << std::endl; // => false
+    auto [it6, flag6] = graph.insert_or_assign_edge({ "first", "second" }, 66.66);
+    std::cout << std::boolalpha << flag6 << std::endl; // => false
+    auto [it7, flag7] = graph.insert_or_assign_edge({ "second", "first" }, 77.77);
+    std::cout << std::boolalpha << flag7 << std::endl; // => true
+    print(graph);
+    auto graph_other = graph; // Конструктор копирования
+    auto graph_new = std::move(graph); // Конструктор перемещения
+    graph = std::move(graph_new); // Перемещающее присваивание
+    graph_new = graph; // Копирующее присваивание
+    graph.swap(graph_new); // Поменять местами содержимое графов
+    swap(graph, graph_new); // Поменять местами содержимое графов
+    print(graph);
+    for (auto& [key, node] : graph) {
+        std::cout << "Is here no edges?" << std::boolalpha << node.empty() <<
+                  std::endl;
+        std::cout << "How many edges are going from here?" << node.size() <<
+                  std::endl;
+        node.value() = Point{ 1,2,3 }; // могу поменять вес
+        for (auto& [key1, weight] : node) {
+                            // key = "new key"; // ОШИБКА: нельзя менять ключ
+            weight = 11.11; // могу задать новый вес у этого ребра
+        }
+        bool flag = node.erase_edge("first"); // => true, если удалил
+// => false, если такого ребра нет
+    }
+    print(graph);
+    bool flag8 = graph.erase_node("new name");
+    std::cout << std::boolalpha << flag8 << std::endl;
+    bool flag9 = graph.erase_node("first");
+    std::cout << std::boolalpha << flag9 << std::endl;
+    print(graph);
+    graph.insert_edge({ "second", "zero" }, 4.4);
+    graph.insert_edge({ "third", "second" }, 6.6);
+    print(graph);
+    bool flag10 = graph.erase_edges_go_from("new name");
+    std::cout << std::boolalpha << flag10 << std::endl;
+    bool flag11 = graph.erase_edges_go_from("second");
+    std::cout << std::boolalpha << flag11 << std::endl;
+    print(graph);
+    graph.erase_edges_go_to("second");
+    print(graph);
+    graph.insert_edge({ "second", "zero" }, 4.4);
+    graph.insert_edge({ "third", "second" }, 6.6);
+    graph.insert_edge({ "third", "third" }, 6.6);
+    graph.insert_edge({ "third", "zero" }, 6.6);
+    print(graph);
+    std::cout << graph.degree_in("second") << std::endl;
+    std::cout << graph.degree_in("third") << std::endl;
+    std::cout << graph.degree_out("second") << std::endl;
+    std::cout << graph.degree_out("third") << std::endl;
+    std::cout << std::boolalpha << graph.loop("second") << std::endl;
+    std::cout << std::boolalpha << graph.loop("third") << std::endl;
+    bool flag12 = graph.erase_node("new name");
+    std::cout << std::boolalpha << flag12 << std::endl;
+    bool flag13 = graph.erase_node("second");
+    std::cout << std::boolalpha << flag13 << std::endl;
+    print(graph);
+    graph.clear_edges(); // Очистить все рёбра
+    print(graph);
+    graph.clear(); // Очистить все вершины (очевидно, вместе с рёбрами)
+    print(graph);
 
     return 0;
 }
